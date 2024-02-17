880ac7fda68d941df449a9c2a23ee90569e2c040
Liam Connolly <liamconns@outlook.com>
2024-02-17 11:45:42 -0600

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     output: 'export',
-   // Add basePath
-    basePath: '/liam-connolly.github.io',
-  
-  }
-  
-  module.exports = nextConfig
+};
+
+export default nextConfig;
