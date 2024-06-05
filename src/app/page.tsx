925b1597940276ad2fcf7ab18d6269654d487fde
Liam Connolly <liamconns@outlook.com>
2024-06-05 17:29:44 -0500

    gif works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-bone">
-      Hello name is Liam
-      <img src={require("../assets/patrickConstruction.gif")} />
+      Hi! My page is currently under construction
+      <Image
+        src={require("../assets/patrickConstruction.gif")}
+        alt="under construction"
+      />
     </main>
   );
 }
