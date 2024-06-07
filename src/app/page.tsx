10ef78f9570707a785de090bbb8833500fa14a27
Liam Connolly <liamconns@outlook.com>
2024-06-07 13:38:46 -0500

    globe + title change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Name from "./components/Name";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24 bg-strokesBlue">
+    <main className="flex flex-col gap-10 min-h-screen items-center justify-center p-24 bg-strokesBlue">
       <Name />
+
+      <Image alt="test" src={require("../assets/globe.gif")} />
     </main>
   );
 }
