ca3bed3119409f9541e7452be9ee19be45925474
Liam Connolly <liamconns@outlook.com>
2024-06-12 12:06:25 -0500

    adding expanding button

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,48 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface NavbarProps {
   onButtonClick: () => void;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ onButtonClick }) => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const handleTimeClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const formattedTime = currentTime.toLocaleTimeString();
+
   return (
-    <div className="absolute top-0 left-0 w-full bg-navGrey p-1 border-2 border-b-black border-r-black border-t-white border-l-white">
+    <div className="absolute h-8 top-0 left-0 w-full bg-navGrey  border-2 border-b-black border-r-black border-t-white border-l-white">
       <button
         className={
-          "px-2 border-2 border-b-black border-r-black border-t-white border-l-white  text-black"
+          "absolute left-2 top-1 px-2 border-2 border-b-black border-r-black border-t-white border-l-white text-black"
         }
-        style={{ fontSize: "12px" }}
+        style={{ fontSize: "11px" }}
         onClick={onButtonClick}
       >
         LIAM CONNOLLY
+      </button>
+      <button
+        className={
+          "absolute right-2 top-1 z-10 px-2 border-2 border-b-black border-r-black border-t-white border-l-white text-black"
+        }
+        style={{ fontSize: "11px" }}
+        onClick={handleTimeClick}
+      >
+        {formattedTime}
+        {showDropdown && <div className=" bg-white  shadow-lg">Hello!</div>}
       </button>
     </div>
   );
